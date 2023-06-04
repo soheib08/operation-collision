016827664fb03b18fcacb49d81688e648a9b85c6
soheib <soheib.mohammadi95@gmail.com>
2023-06-05 02:54:59 +0330

    add tests

--- src/operation.ts
+++ src/operation.ts
@@ -7,59 +7,9 @@ export class Operation {
       this.edits = edits;
     }
   
-    combine(operation: Operation): void {
-      const combinedEdits: Edit[] = [];
-      let position1 = 0;
-      let position2 = 0;
-  
-      while (position1 < this.edits.length && position2 < operation.edits.length) {
-        const edit1 = this.edits[position1];
-        const edit2 = operation.edits[position2];
-  
-        if ("skip" in edit1 && "skip" in edit2) {
-          combinedEdits.push({ skip: edit1.skip + edit2.skip });
-          position1++;
-          position2++;
-        } else if ("insert" in edit1 && "insert" in edit2) {
-          combinedEdits.push(edit1);
-          position1++;
-          position2++;
-        } else if ("delete" in edit1 && "delete" in edit2) {
-          combinedEdits.push({ delete: edit1.delete + edit2.delete });
-          position1++;
-          position2++;
-        } else if ("skip" in edit1) {
-          combinedEdits.push(edit1);
-          position1++;
-        } else if ("skip" in edit2) {
-          combinedEdits.push(edit2);
-          position2++;
-        } else if ("insert" in edit1) {
-          combinedEdits.push(edit1);
-          position1++;
-        } else if ("insert" in edit2) {
-          combinedEdits.push(edit2);
-          position2++;
-        } else if ("delete" in edit1) {
-          combinedEdits.push(edit1);
-          position1++;
-        } else if ("delete" in edit2) {
-          combinedEdits.push(edit2);
-          position2++;
-        }
-      }
-  
-      while (position1 < this.edits.length) {
-        combinedEdits.push(this.edits[position1]);
-        position1++;
-      }
-  
-      while (position2 < operation.edits.length) {
-        combinedEdits.push(operation.edits[position2]);
-        position2++;
-      }
-  
-      this.edits = combinedEdits;
+    combine(operation: Operation): Operation {
+      const combinedEdits = [...this.edits, ...operation.edits];
+      return new Operation(combinedEdits);
     }
   
     static combine(op1: Operation, op2: Operation): Operation {
@@ -86,3 +36,4 @@ export class Operation {
       return result;
     }
   }
+  
