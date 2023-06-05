c7d03182c459de72fc525a93b1ae00a0cc4f517e
soheib <soheib.mohammadi95@gmail.com>
2023-06-05 17:04:53 +0330

    add delete operation test

--- src/operation.spec.ts
+++ src/operation.spec.ts
@@ -6,6 +6,7 @@ describe("Operation", () => {
   const op1 = new Operation([{ skip: 1 }, { insert: "FOO" }]);
   const op2 = new Operation([{ skip: 3 }, { insert: "BAR" }]);
   const op3 = new Operation([{ skip: 2 }, { insert: "BAR" }]);
+  const op4 = new Operation([{ skip: 3 }, { delete: 2 }]);
 
   it("should apply an operation correctly", () => {
     expect(op1.apply(s)).to.equal("aFOObcdefg");
@@ -18,5 +19,16 @@ describe("Operation", () => {
 
     const combined2 = Operation.combine(op3, op1);
     expect(combined2.apply(s)).to.equal("abBARcFOOdefg");
+  });
+
+  it("should combine two operations correctly with delete operation example", () => {
+    expect(op1.apply(s)).to.equal("aFOObcdefg");
+    expect(op4.apply(s)).to.equal("abcfg");
+
+    const combined1 = Operation.combine(op1, op4);
+    expect(combined1.apply(s)).to.equal("aFOObcdg");
+
+    const combined2 = Operation.combine(op4, op1);
+    expect(combined2.apply(s)).to.equal("abcfFOOg");
   });
 });
